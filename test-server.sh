1e355fb6b459ad53f9316dca32b929b9e4854da3
Lexxy <lexxy23@users.noreply.github.com>
2023-04-19 10:41:44 +0200

    👷 add script to test server up

---
+++ test-server.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+MM_SERVER_URL='http://localhost:16020/'
+
+echo "$(date): waiting for server to come up.."
+SERVER_UP=1
+for i in $(seq 1 60); do
+    curl -o /dev/null -L $MM_SERVER_URL 2>>/dev/null
+    RC=$?
+    if [[ $RC -gt 0 ]]; then
+        echo -n "."
+        sleep 2
+    else
+        echo "works"
+        SERVER_UP=0
+        break
+    fi
+done
+
+if [[ $SERVER_UP -eq 0 ]]; then
+    echo "$(date): is running"
+else
+    echo "$(date): looks like there are startup issues. Please check log"
+    exit 1
+fi
