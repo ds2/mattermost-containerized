ed36728a96c1aaf933a9da3c80ccd0e77ca99fa4
Lexxy <lexxy23@users.noreply.github.com>
2023-04-19 11:23:16 +0200

    💚 print out compose logs in case of error

--- test-server.sh
+++ test-server.sh
@@ -5,7 +5,7 @@ MM_SERVER_URL='http://localhost:16020/'
 echo "$(date): waiting for server to come up.."
 SERVER_UP=1
 for i in $(seq 1 90); do
-    curl -o /dev/null -L $MM_SERVER_URL 2>>/dev/null
+    curl -o /dev/null -L $MM_SERVER_URL 2>>test-server.log
     RC=$?
     if [[ $RC -gt 0 ]]; then
         echo -n "."
@@ -21,5 +21,8 @@ if [[ $SERVER_UP -eq 0 ]]; then
     echo "$(date): is running"
 else
     echo "$(date): looks like there are startup issues. Please check log"
+    docker-compose logs --tail=40
+    echo "test-server log from curl is here:"
+    cat test-server.log
     exit 1
 fi
