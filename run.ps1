2ddd7728fdcd78c561a7ecc574119aeaf629c836
Lexxy <lexxy23@noreply.codeberg.org>
2025-06-01 14:38:30 +0200

    build(windows): add instructions to run MM in Windows

---
+++ run.ps1
@@ -0,0 +1,31 @@
+#!/usr/bin/env pwsh
+#
+# shell script to run the Mattermost containerized application in windows using podman
+#
+#
+$thisDir = Get-Location
+
+$volumes = @("config","data","logs","plugins","client/plugins","bleve-indexes")
+$volumes | ForEach-Object {
+    New-Item -ItemType Directory -Path ./volumes/app/mattermost/$_ -Force
+}
+
+podman compose version
+
+# set DB_PW
+$env:DB_PW = "your_password_here"
+
+# check if $DB_PW is defined
+if (-not $env:DB_PW) {
+    Write-Output "DB_PW is not defined"
+    exit 1
+}
+
+Write-Output "Starting Mattermost containerized application..."
+podman compose -f container-compose.yaml up --remove-orphans --detach
+
+Write-Output "Mattermost containerized application started"
+Write-Output "To stop the application, run 'podman compose -f container-compose.yaml down'"
+
+# open browser
+Start-Process "http://localhost:16020"
